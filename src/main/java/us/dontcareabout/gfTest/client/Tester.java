ec312dccaf5e18a280ea2e6dd0805af761ba53f9
MontyPan <monty.pan@dontcareabout.us>
2018-03-10 15:22:26 +0800

    GWT 基礎檔案

---
+++ src/main/java/us/dontcareabout/gfTest/client/Tester.java
@@ -0,0 +1,28 @@
+package us.dontcareabout.gfTest.client;
+
+import com.google.gwt.user.client.Window;
+
+import us.dontcareabout.gwt.client.GFEP;
+import us.dontcareabout.gwt.client.iCanUse.Feature;
+
+public class Tester extends GFEP {
+	public Tester() {
+		needFeature(Feature.Canvas);
+	}
+
+	@Override
+	protected String version() { return "0.0.1"; }
+
+	@Override
+	protected String defaultLocale() { return "zh_TW"; }
+
+
+	@Override
+	protected void featureFail() {
+		Window.alert("這個瀏覽器我不尬意，不給用..... \\囧/");
+	}
+
+	@Override
+	protected void start() {
+	}
+}
