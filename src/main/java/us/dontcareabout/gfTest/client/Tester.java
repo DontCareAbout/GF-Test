77b192d50172d95a329feea53c7d9702d4ba80ee
MontyPan <monty.pan@dontcareabout.us>
2018-04-08 10:58:24 +0800

    FontMetrics 基本測試

--- src/main/java/us/dontcareabout/gfTest/client/Tester.java
+++ src/main/java/us/dontcareabout/gfTest/client/Tester.java
@@ -1,10 +1,11 @@
 package us.dontcareabout.gfTest.client;
 
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.sencha.gxt.widget.core.client.container.Viewport;
 
-import us.dontcareabout.gfTest.client.ui.MainView;
+import us.dontcareabout.fontMetrics.client.Font;
+import us.dontcareabout.fontMetrics.client.FontMetrics;
+import us.dontcareabout.fontMetrics.client.MetricsField;
+import us.dontcareabout.gwt.client.Console;
 import us.dontcareabout.gwt.client.GFEP;
 import us.dontcareabout.gwt.client.iCanUse.Feature;
 
@@ -27,8 +28,35 @@ public class Tester extends GFEP {
 
 	@Override
 	protected void start() {
-		Viewport vp = new Viewport();
-		vp.add(new MainView());
-		RootPanel.get().add(vp);
+		FontMetrics fm = FontMetrics.get(new Font());
+		Console.log(fm.getValue(MetricsField.bottom));
+		if (!TIMES_200.equals(fm)) {
+			Window.alert(diff(TIMES_200, fm));
+		} else {
+			Window.alert("\\囧/");
+		}
+	}
+
+	private static final FontMetrics TIMES_200 = new FontMetrics();
+	static {
+		TIMES_200.setValue(MetricsField.bottom, 0.5);
+		TIMES_200.setValue(MetricsField.capHeight, -0.68);
+		TIMES_200.setValue(MetricsField.baseline, 0);
+		TIMES_200.setValue(MetricsField.xHeight, -0.45);
+		TIMES_200.setValue(MetricsField.descent, 0.21);
+		TIMES_200.setValue(MetricsField.ascent, -0.695);
+		TIMES_200.setValue(MetricsField.tittle, -0.695);
+	}
+
+	private static String diff(FontMetrics a, FontMetrics b) {
+		StringBuffer result = new StringBuffer();
+
+		for (MetricsField type : MetricsField.values()) {
+			if (a.getValue(type) != b.getValue(type)) {
+				result.append(type + " : " + a.getValue(type) + " != " + b.getValue(type));
+			}
+		}
+
+		return result.toString();
 	}
 }
