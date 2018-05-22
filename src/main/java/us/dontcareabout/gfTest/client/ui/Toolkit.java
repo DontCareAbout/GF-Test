5b4032583e7df6f045d5cfe024129f9b946727c8
MontyPan <monty.pan@dontcareabout.us>
2018-05-22 16:30:54 +0800

    增加：產生 TextButton 的 util

--- src/main/java/us/dontcareabout/gfTest/client/ui/Toolkit.java
+++ src/main/java/us/dontcareabout/gfTest/client/ui/Toolkit.java
@@ -1,9 +1,11 @@
 package us.dontcareabout.gfTest.client.ui;
 
 import com.google.gwt.user.client.Random;
+import com.sencha.gxt.chart.client.draw.Color;
 import com.sencha.gxt.chart.client.draw.RGB;
 
 import us.dontcareabout.gxt.client.draw.LRectangleSprite;
+import us.dontcareabout.gxt.client.draw.component.TextButton;
 
 public class Toolkit {
 	public static LRectangleSprite genRectangle() {
@@ -19,6 +21,18 @@ public class Toolkit {
 		result.setFill(color);
 		result.setStroke(new RGB(256 - color.getRed(), 256 - color.getGreen(), 256 - color.getBlue()));
 		result.setStrokeWidth(2);
+		return result;
+	}
+
+	public static TextButton genButton(String text) {
+		return genButton(text, RGB.WHITE, RGB.BLACK);
+	}
+
+	public static TextButton genButton(String text, Color textColor, Color bgColor) {
+		TextButton result = new TextButton(text);
+		result.setTextColor(textColor);
+		result.setBgColor(bgColor);
+		result.setBgRadius(10);
 		return result;
 	}
 }
